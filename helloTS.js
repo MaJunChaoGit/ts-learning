2821051c14b07237f36249fbf889f0f6cbeafd73
MaJunChaoGit <354488954@qq.com>
2019-03-23 23:35:22 +0800

    学到了接口

---
+++ helloTS.js
@@ -0,0 +1,24 @@
+function sayHello(person) {
+    return "Hello," + person;
+}
+var user = 'Tom';
+console.log(sayHello(user));
+var isDone = false;
+var createdByNewBoolean = new Boolean(1);
+var flag = Boolean(1);
+var deLiterral = 5;
+var myName = "Ma" + 'junchao';
+var eunusable = undefined;
+var _name = 'mjc';
+function getLength(something) {
+    return something.toString();
+}
+getLength(_name);
+var tom = {
+    name: 'Tom',
+    age: 123
+};
+var tiger = {
+    name: ''
+};
+tiger.name == '渣渣';
---
+++ helloTS.ts
@@ -0,0 +1,45 @@
+function sayHello(person: string) {
+  return `Hello,${person}`;
+}
+let user = 'Tom';
+console.log(sayHello(user));
+
+let isDone: boolean = false;
+
+let createdByNewBoolean: Boolean = new Boolean(1);
+
+let flag: boolean = Boolean(1);
+
+let deLiterral: number = 5;
+
+let myName: string = `Ma` + 'junchao';
+
+let eunusable: void = undefined;
+
+let _name: string = 'mjc';
+
+function getLength(something: string): string {
+  return something.toString();
+}
+
+getLength(_name)
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+let tom: Person = {
+  name: 'Tom',
+  age: 123,
+}
+
+interface Animal {
+  readonly name: string;
+}
+
+let tiger: Animal = {
+  name: ''
+}
+
+tiger.name == '渣渣';
